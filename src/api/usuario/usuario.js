31e0787251d3e48d4a57859b55b6216f0fb998dc
David Pasco <cpasco@gmail.com>
2017-10-24 19:22:47 -0200

    Tratamento das chaves de api.

--- src/api/usuario/usuario.js
+++ src/api/usuario/usuario.js
@@ -3,12 +3,14 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
+var keys = require('../keys');
+
 router.post('/:cpf/validar', function (req, res) {
   var config = {
     headers: {
       'Content-Type': 'application/json',
-      'app_id': 'efe6cc00',
-      'app_key': 'f86cadc55991210addba87d1d8736658',
+      'app_id': keys.app_id,
+      'app_key': keys.app_key,
     }
   };
 
@@ -37,7 +39,7 @@ router.post('/:cpf/imagem', function (req, res) {
       'app_key': 'f86cadc55991210addba87d1d8736658',
     }
   };
-console.log('Imagem: '+req.body.imagem)
+  
   var payload = {
     "image": req.body.imagem,
     "gallery_name":"gal-" + req.params.cpf,

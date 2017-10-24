753297ab991ef6b4a9596cb6876095af66f640b5
David Pasco <cpasco@gmail.com>
2017-10-24 20:33:04 -0200

    Implementação POST /usuario.

--- src/api/usuario/usuario.js
+++ src/api/usuario/usuario.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var MongoClient = require('mongodb').MongoClient;
 
 var keys = require('../keys');
 
@@ -55,6 +56,31 @@ router.post('/:cpf/imagem', function (req, res) {
       console.log(err);
       res.send(err);
     });  
+});
+
+router.post('/', function (req, res){
+  var currentTime = new Date().getTime();
+  var user = {
+    nome: req.body.nome,
+    nmBeneficio: req.body.nm_beneficio,
+    cpf: req.body.cpf,
+    dtNascimento: req.body.dt_nascimento,
+    createdAt: currentTime,
+    updatedAt: currentTime
+  }
+
+  MongoClient.connect('mongodb://192.168.0.142:27017/local', function(err, db) {
+    if (err) {
+      throw err;
+    }
+    db.collection('user').insert(user, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      res.send(result)
+    });
+  });
 });
 
 module.exports = router;

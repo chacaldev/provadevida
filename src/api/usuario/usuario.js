034e88368f6a59241fbf6202c6dd6f90c4347683
David Pasco <cpasco@gmail.com>
2017-10-24 18:59:59 -0200

    Implementação /usuario/:cpf/{imagem|validar}

---
+++ src/api/usuario/usuario.js
@@ -0,0 +1,58 @@
+'use strict'
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+router.post('/:cpf/validar', function (req, res) {
+  var config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'app_id': 'efe6cc00',
+      'app_key': 'f86cadc55991210addba87d1d8736658',
+    }
+  };
+
+  var payload = {
+    "image": req.body.imagem,
+    "gallery_name":"gal-" + req.params.cpf,
+    "subject_id": req.params.cpf
+  };
+
+  axios.post('https://api.kairos.com/verify', payload, config)
+    .then(function(resp) {
+      console.log(resp.data);
+      res.send(resp.data);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.send(err);
+    });  
+});
+
+router.post('/:cpf/imagem', function (req, res) {
+  var config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'app_id': 'efe6cc00',
+      'app_key': 'f86cadc55991210addba87d1d8736658',
+    }
+  };
+console.log('Imagem: '+req.body.imagem)
+  var payload = {
+    "image": req.body.imagem,
+    "gallery_name":"gal-" + req.params.cpf,
+    "subject_id": req.params.cpf
+  };
+
+  axios.post('https://api.kairos.com/enroll', payload, config)
+    .then(function(resp) {
+      console.log(resp.data);
+      res.send(resp.data);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.send(err);
+    });  
+});
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 var hello = require('./api/hello/hello_get');
+var usuario = require('./api/usuario/usuario');
 
 var myLogger = function (req, res, next) {
   console.log('LOGGED');
@@ -9,6 +12,9 @@ var myLogger = function (req, res, next) {
 
 app.use(myLogger);
 
-app.use('/hello', hello);
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/hello', hello);
+app.use('/usuario', usuario);
 app.listen(3000);
